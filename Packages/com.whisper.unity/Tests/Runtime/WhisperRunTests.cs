012ae52508e1d19978acb992b45b4571ec0756bc
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-04-09 12:28:35 +0200

    Add basic smoke test

--- Packages/com.whisper.unity/Tests/Runtime/WhisperRunTests.cs
+++ Packages/com.whisper.unity/Tests/Runtime/WhisperRunTests.cs
@@ -1,7 +1,47 @@
+using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UnityEngine;
+
 namespace Whisper.Tests
 {
     public class WhisperRunTests
     {
+        private readonly string _modelPath = Path.Combine(Application.streamingAssetsPath, "Whisper/ggml-tiny.bin");
+        private readonly float[] _buffer = new float[16000];
+        private const int Frequency = 16000;
+        private const int Channels = 1;
+
+        private WhisperWrapper _whisper;
+        private WhisperParams _params;
         
+        [SetUp]
+        public void Setup()
+        {
+            _whisper = WhisperWrapper.InitFromFile(_modelPath);
+            _params = WhisperParams.GetDefaultParams();
+        }
+        
+        [Test]
+        public void GetTextTest()
+        {
+            var res = _whisper.GetText(_buffer, Frequency, Channels, _params);
+            Assert.NotNull(res);
+
+            var clip = AudioClip.Create("test", _buffer.Length, Channels, Frequency, false);
+            res = _whisper.GetText(clip, _params);
+            Assert.NotNull(res);
+        }
+        
+        [Test]
+        public async Task GetTextTestAsync()
+        {
+            var res = await _whisper.GetTextAsync(_buffer, Frequency, Channels, _params);
+            Assert.NotNull(res);
+            
+            var clip = AudioClip.Create("test", _buffer.Length, Channels, Frequency, false);
+            res = await _whisper.GetTextAsync(clip, _params);
+            Assert.NotNull(res);
+        }
     }
 }
