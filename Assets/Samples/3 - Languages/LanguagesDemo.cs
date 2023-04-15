9a77ab7380d4a62fe1faef34eb6f830b3cbfcb82
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-04-15 02:43:38 +0200

    Modified demo

--- Assets/Samples/3 - Languages/LanguagesDemo.cs
+++ Assets/Samples/3 - Languages/LanguagesDemo.cs
@@ -18,18 +18,24 @@ namespace Whisper.Samples
         private void UpdateText()
         {
             var sb = new StringBuilder();
-            sb.AppendLine("All languages names:");
-
-            // write all languages in one string
-            var languages = WhisperLanguage.GetAllLanguages();
-            for (var i = 0; i < languages.Length - 1; i++)
-                sb.Append(languages[i] + ", ");
-            sb.Append(languages[languages.Length - 1] + ".\n\n");
-
+            
             // check if Multilingual
             var multi = whisper.IsMultilingual();
             var msg = "Current model Multilingual: " + multi;
             sb.AppendLine(msg);
+
+            if (multi)
+            {
+                sb.AppendLine();
+                sb.AppendLine("All languages names:");
+
+                // write all languages in one string
+                var languages = WhisperLanguage.GetAllLanguages();
+                for (var i = 0; i < languages.Length - 1; i++)
+                    sb.Append(languages[i] + ", ");
+                sb.Append(languages[languages.Length - 1] + ".");
+            }
+
 
             text.text = sb.ToString();
         }
