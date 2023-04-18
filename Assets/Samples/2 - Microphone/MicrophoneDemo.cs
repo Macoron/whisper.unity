3406e5ee87eb51c974dfb6b350aeb7daa6dd5241
Ravil <graf.rav@gmail.com>
2023-04-18 17:09:59 +0300

    MicrophoneRecord class extracted (#11)

--- Assets/Samples/2 - Microphone/MicrophoneDemo.cs
+++ Assets/Samples/2 - Microphone/MicrophoneDemo.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
@@ -10,77 +8,63 @@ namespace Whisper.Samples
     public class MicrophoneDemo : MonoBehaviour
     {
         public WhisperManager whisper;
+        public MicrophoneRecord microphoneRecord;
         public bool streamSegments = true;
         public bool printLanguage = true;
-
-        [Header("Mic settings")] 
-        public int maxLengthSec = 30;
-        public int frequency = 16000;
-        public bool echo = true;
 
         [Header("UI")] 
         public Button button;
         public Text buttonText;
         public Text outputText;
         public Text timeText;
-        private const string MicrophoneDefaultLabel = "Default mic";
         public Dropdown microphoneDropdown;
         public Dropdown languageDropdown;
         public Toggle translateToggle;
 
-        private float _recordStart;
-        private bool _isRecording;
-        private AudioClip _clip;
+        private const string MicrophoneDefaultLabel = "Default mic";
+        
         private string _buffer;
-        private float _length;
-        private string _selectedMicDevice;
-        private string _recordStartMicDevice;
 
         private void Awake()
         {
             button.onClick.AddListener(OnButtonPressed);
 
-            microphoneDropdown.options = new List<Dropdown.OptionData> { new(MicrophoneDefaultLabel) };
-            microphoneDropdown.options.AddRange(Microphone.devices.Select(micName => new Dropdown.OptionData(micName)));
-            microphoneDropdown.value = microphoneDropdown.options.FindIndex(op => op.text == MicrophoneDefaultLabel);
+            microphoneDropdown.options = microphoneRecord.AvailableMicDevices
+                .Prepend(MicrophoneDefaultLabel)
+                .Select(text => new Dropdown.OptionData(text))
+                .ToList();
+            microphoneDropdown.value = microphoneDropdown.options
+                .FindIndex(op => op.text == MicrophoneDefaultLabel);
             microphoneDropdown.onValueChanged.AddListener(OnMicrophoneChanged);
             
             languageDropdown.value = languageDropdown.options
-                .FindIndex((op) => op.text == whisper.language);
+                .FindIndex(op => op.text == whisper.language);
             languageDropdown.onValueChanged.AddListener(OnLanguageChanged);
 
             translateToggle.isOn = whisper.translateToEnglish;
             translateToggle.onValueChanged.AddListener(OnTranslateChanged);
+
+            microphoneRecord.OnRecordStop += Transcribe;
             
             if (streamSegments)
                 whisper.OnNewSegment += WhisperOnOnNewSegment;
         }
-        
-        private void Update()
-        {
-            if (!_isRecording)
-                return;
 
-            var timePassed = Time.realtimeSinceStartup - _recordStart;
-            if (timePassed > maxLengthSec)
-                StopRecord();
-        }
-
-        public void OnButtonPressed()
+        private void OnButtonPressed()
         {
-            if (!_isRecording)
-                StartRecord();
+            if (!microphoneRecord.IsRecording)
+                microphoneRecord.StartRecord();
             else
-                StopRecord();
+                microphoneRecord.StopRecord();
 
             if (buttonText)
-                buttonText.text = _isRecording ? "Stop" : "Record";
+                buttonText.text = microphoneRecord.IsRecording ? "Stop" : "Record";
         }
 
         private void OnMicrophoneChanged(int ind)
         {
             var opt = microphoneDropdown.options[ind];
-            _selectedMicDevice = opt.text == MicrophoneDefaultLabel ? null : opt.text;
+            microphoneRecord.SelectedMicDevice = opt.text == MicrophoneDefaultLabel ? null : opt.text;
         }
         
         private void OnLanguageChanged(int ind)
@@ -94,66 +78,17 @@ namespace Whisper.Samples
             whisper.translateToEnglish = translate;
         }
 
-        public void StartRecord()
-        {
-            if (_isRecording)
-                return;
-
-            _recordStart = Time.realtimeSinceStartup;
-            _recordStartMicDevice = _selectedMicDevice;
-            _clip = Microphone.Start(_recordStartMicDevice, false, maxLengthSec, frequency);
-            _isRecording = true;
-        }
-
-        public void StopRecord()
-        {
-            if (!_isRecording)
-                return;
-
-            var data = GetTrimmedData();
-            if (echo)
-            {
-                var echoClip = AudioClip.Create("echo", data.Length,
-                    _clip.channels, _clip.frequency, false);
-                echoClip.SetData(data, 0);
-                AudioSource.PlayClipAtPoint(echoClip, Vector3.zero);
-            }
-
-            Microphone.End(_recordStartMicDevice);
-            _isRecording = false;
-            _length = Time.realtimeSinceStartup - _recordStart;
-
-            Transcribe(data);
-        }
-
-        private float[] GetTrimmedData()
-        {
-            // get microphone samples and current position
-            var pos = Microphone.GetPosition(_recordStartMicDevice);
-            var origData = new float[_clip.samples * _clip.channels];
-            _clip.GetData(origData, 0);
-
-            // check if mic just reached audio buffer end
-            if (pos == 0)
-                return origData;
-
-            // looks like we need to trim it by pos
-            var trimData = new float[pos];
-            Array.Copy(origData, trimData, pos);
-            return trimData;
-        }
-
-        private async void Transcribe(float[] data)
+        private async void Transcribe(float[] data, AudioClip clip, float length)
         {
             _buffer = "";
             
             var sw = new Stopwatch();
             sw.Start();
             
-            var res = await whisper.GetTextAsync(data, _clip.frequency, _clip.channels);
+            var res = await whisper.GetTextAsync(data, clip.frequency, clip.channels);
 
             var time = sw.ElapsedMilliseconds;
-            var rate =_length / (time * 0.001f);
+            var rate = length / (time * 0.001f);
             timeText.text = $"Time: {time} ms\nRate: {rate:F1}x";
             if (res == null)
                 return;
---
+++ Assets/Samples/2 - Microphone/MicrophoneRecord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Whisper.Samples
+{
+    public class MicrophoneRecord : MonoBehaviour
+    {
+        [Header("Mic settings")]
+        public int maxLengthSec = 30;
+        public int frequency = 16000;
+        public bool echo = true;
+        
+        private float _recordStart;
+        private AudioClip _clip;
+        private float _length;
+
+        private string _selectedMicDevice;
+        public string SelectedMicDevice
+        {
+            get => _selectedMicDevice;
+            set
+            {
+                if (value != null && !AvailableMicDevices.Contains(value))
+                    throw new ArgumentException("Microphone device not found");
+                _selectedMicDevice = value;
+            }
+        }
+
+        public string RecordStartMicDevice { get; private set; }
+        public bool IsRecording { get; private set; }
+
+        public IEnumerable<string> AvailableMicDevices => Microphone.devices;
+
+        private void Update()
+        {
+            if (!IsRecording)
+                return;
+
+            var timePassed = Time.realtimeSinceStartup - _recordStart;
+            if (timePassed > maxLengthSec)
+                StopRecord();
+        }
+
+        public void StartRecord()
+        {
+            if (IsRecording)
+                return;
+
+            _recordStart = Time.realtimeSinceStartup;
+            RecordStartMicDevice = SelectedMicDevice;
+            _clip = Microphone.Start(RecordStartMicDevice, false, maxLengthSec, frequency);
+            IsRecording = true;
+        }
+
+        public void StopRecord()
+        {
+            if (!IsRecording)
+                return;
+
+            var data = GetTrimmedData();
+            if (echo)
+            {
+                var echoClip = AudioClip.Create("echo", data.Length,
+                    _clip.channels, _clip.frequency, false);
+                echoClip.SetData(data, 0);
+                AudioSource.PlayClipAtPoint(echoClip, Vector3.zero);
+            }
+
+            Microphone.End(RecordStartMicDevice);
+            IsRecording = false;
+            _length = Time.realtimeSinceStartup - _recordStart;
+
+            OnRecordStop?.Invoke(data, _clip, _length);
+        }
+        
+        public delegate void OnRecordStopDelegate(float[] data, AudioClip clip, float length);
+        public event OnRecordStopDelegate OnRecordStop;
+
+        private float[] GetTrimmedData()
+        {
+            // get microphone samples and current position
+            var pos = Microphone.GetPosition(RecordStartMicDevice);
+            var origData = new float[_clip.samples * _clip.channels];
+            _clip.GetData(origData, 0);
+
+            // check if mic just reached audio buffer end
+            if (pos == 0)
+                return origData;
+
+            // looks like we need to trim it by pos
+            var trimData = new float[pos];
+            Array.Copy(origData, trimData, pos);
+            return trimData;
+        }
+    }
+}
