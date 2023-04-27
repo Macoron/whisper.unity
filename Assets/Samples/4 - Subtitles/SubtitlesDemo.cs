0a138dcd8c1c444448b020ec99a2f9b4f1ab7ba6
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-04-27 23:40:05 +0200

    Finished subs demo

--- Assets/Samples/4 - Subtitles/SubtitlesDemo.cs
+++ Assets/Samples/4 - Subtitles/SubtitlesDemo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -20,24 +21,54 @@ namespace Whisper.Samples
 
         private void Awake()
         {
+            languageDropdown.value = languageDropdown.options
+                .FindIndex(op => op.text == whisper.language);
+            languageDropdown.onValueChanged.AddListener(OnLanguageChanged);
+
+            translateToggle.isOn = whisper.translateToEnglish;
+            translateToggle.onValueChanged.AddListener(OnTranslateChanged);
+            
             button.onClick.AddListener(OnButtonPressed);
+        }
+        
+        private void OnLanguageChanged(int ind)
+        {
+            var opt = languageDropdown.options[ind];
+            whisper.language = opt.text;
+        }
+        
+        private void OnTranslateChanged(bool translate)
+        {
+            whisper.translateToEnglish = translate;
         }
 
         private async void OnButtonPressed()
         {
+            outputText.text = "Transcribing...";
+            var sw = new Stopwatch();
+            sw.Start();
+            
             var res = await whisper.GetTextAsync(clip);
+            
+            var time = sw.ElapsedMilliseconds;
+            var rate = clip.length / (time * 0.001f);
+            timeText.text = $"Time: {time} ms\nRate: {rate:F1}x";
 
+            // start playing sound
             var go = new GameObject();
             var source = go.AddComponent<AudioSource>();
             source.clip = clip;
             source.Play();
 
+            // and show subtitles at the same time
             while (source.time < clip.length)
             {
                 var text = GetSubtitles(res, source.time);
                 outputText.text = text;
                 await Task.Yield();
             }
+
+            outputText.text = ResultToRichText(res);
         }
 
         // TODO: this isn't optimized and for demo use only
@@ -47,21 +78,66 @@ namespace Whisper.Samples
             var time = TimeSpan.FromSeconds(timeSec);
             foreach (var seg in res.Segments)
             {
-                if (time > seg.End)
+                // check if we already passed whole segment
+                if (time >= seg.End)
                 {
-                    sb.Append(seg.Text);
+                    sb.Append(SegmentToRichText(seg));
                     continue;
                 }
 
                 foreach (var token in seg.Tokens)
                 {
                     if (time > token.Start)
-                        sb.Append(token.Text);
-
+                    {
+                        var text = TokenToRichText(token);
+                        sb.Append(text);
+                    }
                 }
             }
 
             return sb.ToString();
+        }
+
+        private static string ResultToRichText(WhisperResult result)
+        {
+            var sb = new StringBuilder();
+            foreach (var seg in result.Segments)
+            {
+                var str = SegmentToRichText(seg);
+                sb.Append(str);
+            }
+
+            return sb.ToString();
+        }
+
+        private static string SegmentToRichText(WhisperSegment segment)
+        {
+            var sb = new StringBuilder();
+            foreach (var token in segment.Tokens)
+            {
+                var tokenText = TokenToRichText(token);
+                sb.Append(tokenText);
+            }
+
+            return sb.ToString();
+        }
+        
+        private static string TokenToRichText(WhisperTokenData token)
+        {
+            var text = token.Text;
+            var textColor = ProbabilityToColor(token.Prob);
+            var richText = $"<color={textColor}>{text}</color>";
+            return richText;
+        }
+        
+        private static string ProbabilityToColor(float p)
+        {
+            if (p <= 0.33f)
+                return "red";
+            else if (p <= 0.66f)
+                return "yellow";
+            else
+                return "green";
         }
     }
 }
