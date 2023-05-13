9c4a80cbb87be789734e742a6aff71f29758038c
Alex Evgrashin <aevgrashin@yandex.ru>
2023-05-13 13:25:25 +0300

    Add linux support

---
+++ build_cpp_linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+whisper_path="$1"
+targets=${2:-all}
+unity_project="$PWD"
+build_path="$1/build"
+
+clean_build(){
+  rm -rf "$build_path"
+  mkdir "$build_path"
+  cd "$build_path"
+}
+
+build_linux() {
+  clean_build
+  echo "Starting building for Linux..."
+
+  cmake -DCMAKE_BUILD_TYPE=Release ../
+  make
+
+  echo "Build for Linux complete!"
+
+  artifact_path="$build_path/libwhisper.so"
+  target_path="$unity_project/Packages/com.whisper.unity/Plugins/Linux/libwhisper.so"
+  cp "$artifact_path" "$target_path"
+
+  echo "Build files copied to $target_path"
+}
+
+build_linux
