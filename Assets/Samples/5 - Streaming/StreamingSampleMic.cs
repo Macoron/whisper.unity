5e785605c8b163d33e5a0af9578f47d41a38fb60
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-07-25 22:29:00 +0200

    Minor changes to stream

--- Assets/Samples/5 - Streaming/StreamingSampleMic.cs
+++ Assets/Samples/5 - Streaming/StreamingSampleMic.cs
@@ -1,50 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Whisper;
 using Whisper.Utils;
 
-public class StreamingSampleMic : MonoBehaviour
+namespace Whisper.Samples
 {
-    public WhisperManager whisper;
-    public MicrophoneRecord microphoneRecord;
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StreamingSampleMic : MonoBehaviour
+    {
+        public WhisperManager whisper;
+        public MicrophoneRecord microphoneRecord;
     
-    [Header("UI")] 
-    public Button button;
-    public Text buttonText;
-    public Text text;
-    public ScrollRect scroll;
-    private WhisperStream _stream;
+        [Header("UI")] 
+        public Button button;
+        public Text buttonText;
+        public Text text;
+        public ScrollRect scroll;
+        private WhisperStream _stream;
 
-    private async void Start()
-    {
-        _stream = await whisper.CreateStream(microphoneRecord);
-        _stream.OnResultUpdated += OnResult;
-        
-        microphoneRecord.OnRecordStop += OnRecordStop;
-        button.onClick.AddListener(OnButtonPressed);
-    }
-
-    private void OnButtonPressed()
-    {
-        if (!microphoneRecord.IsRecording)
+        private async void Start()
         {
-            _stream.StartStream();
-            microphoneRecord.StartRecord();
-        }
-        else
-            microphoneRecord.StopRecord();
+            _stream = await whisper.CreateStream(microphoneRecord);
+            _stream.OnResultUpdated += OnResult;
+            _stream.OnStreamFinished += OnFinished;
         
-        buttonText.text = microphoneRecord.IsRecording ? "Stop" : "Record";
-    }
+            microphoneRecord.OnRecordStop += OnRecordStop;
+            button.onClick.AddListener(OnButtonPressed);
+        }
+
+        private void OnButtonPressed()
+        {
+            if (!microphoneRecord.IsRecording)
+            {
+                _stream.StartStream();
+                microphoneRecord.StartRecord();
+            }
+            else
+                microphoneRecord.StopRecord();
+        
+            buttonText.text = microphoneRecord.IsRecording ? "Stop" : "Record";
+        }
     
-    private void OnRecordStop(float[] data, int frequency, int channels, float length)
-    {
-        buttonText.text = "Record";
-    }
+        private void OnRecordStop(float[] data, int frequency, int channels, float length)
+        {
+            buttonText.text = "Record";
+        }
     
-    private void OnResult(string result)
-    {
-        text.text = result;
-        UiUtils.ScrollDown(scroll);
+        private void OnResult(string result)
+        {
+            text.text = result;
+            UiUtils.ScrollDown(scroll);
+        }
+        
+        private void OnFinished(string finalResult)
+        {
+            print("Stream finished!");
+        }
     }
 }
