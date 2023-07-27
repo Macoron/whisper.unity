334286811f3b176be473dd95c79a9605b19625aa
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-07-27 23:14:19 +0200

    vad

--- Packages/com.whisper.unity/Runtime/Utils/MicrophoneRecord.cs
+++ Packages/com.whisper.unity/Runtime/Utils/MicrophoneRecord.cs
@@ -29,7 +29,7 @@ namespace Whisper.Utils
         [Header("Voice Activation Detection (VAD)")]
         public bool useVad = true;
         public float vadUpdateRateSec = 0.1f;
-        public float vadContextSec = 5f;
+        public float vadContextSec = 10f;
         public float vadLastSec = 1.25f;
         public float vadThd = 0.6f;
         public float vadFreqThd = 100.0f;
