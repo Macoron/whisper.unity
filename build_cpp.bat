c9030237936c6df9875cf92cee9b9e0c04b7b944
Alex Evgrashin <aevgrashin@yandex.ru>
2023-11-20 21:36:40 +0100

    Changed script to build CUDA

--- build_cpp.bat
+++ build_cpp.bat
@@ -1,9 +1,35 @@
-set MY_PATH=%CD%
+@echo off
 
-cd %1
-rmdir .\build /s /q
-cmake -S . -B ./build -A x64 -DCMAKE_BUILD_TYPE=Release -DWHISPER_BUILD_TESTS=OFF -DWHISPER_BUILD_EXAMPLES=OFF 
+set unity_path=%CD%
+set target=%1
+set whisper_path=%2
 
-cd ./build
-msbuild ALL_BUILD.vcxproj -t:build -p:configuration=Release -p:platform=x64
-xcopy /y /q .\bin\Release\whisper.dll %MY_PATH%\Packages\com.whisper.unity\Plugins\Windows\libwhisper.dll*
+IF "%target%"=="cpu" goto cpu
+IF "%target%"=="cuda" goto cuda
+IF "%target%"=="all" goto cpu
+
+echo Unknown target %target%, should "cpu", "cuda" or "all"
+goto commonexit
+
+:cpu
+	echo Starting building cpu target...
+	cd %whisper_path%
+	rmdir .\build /s /q
+	cmake -S . -B ./build -A x64 -DCMAKE_BUILD_TYPE=Release -DWHISPER_BUILD_TESTS=OFF -DWHISPER_BUILD_EXAMPLES=OFF 
+
+	cd ./build
+	msbuild ALL_BUILD.vcxproj -t:build -p:configuration=Release -p:platform=x64
+	xcopy /y /q .\bin\Release\whisper.dll %unity_path%\Packages\com.whisper.unity\Plugins\Windows\libwhisper.dll*
+
+	IF NOT "%target%"=="all" goto commonexit
+:cuda
+	echo Starting building CUDA target...
+	cd %whisper_path%
+	rmdir .\build /s /q
+	cmake -S . -B ./build -A x64 -DWHISPER_CUBLAS=ON -DCMAKE_BUILD_TYPE=Release -DWHISPER_BUILD_TESTS=OFF -DWHISPER_BUILD_EXAMPLES=OFF 
+
+	cd ./build
+	msbuild ALL_BUILD.vcxproj -t:build -p:configuration=Release -p:platform=x64
+	xcopy /y /q .\bin\Release\whisper.dll %unity_path%\Packages\com.whisper.unity\Plugins\Windows\libwhisper_cuda.dll*
+
+:commonexit
