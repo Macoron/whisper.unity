46271f121bfb995956ce7f6ced46738c463fb8d4
Alexander Evgrashin <aevgrashin@yandex.ru>
2025-04-15 22:19:38 +0200

    Updated build code for Android

--- build_cpp.sh
+++ build_cpp.sh
@@ -71,15 +71,22 @@ build_android() {
   clean_build
   echo "Starting building for Android..."
 
-  cmake -DCMAKE_TOOLCHAIN_FILE="$android_sdk_path" -DANDROID_ABI=arm64-v8a -DBUILD_SHARED_LIBS=OFF \
+  cmake -DCMAKE_TOOLCHAIN_FILE="$android_sdk_path" -DANDROID_ABI=arm64-v8a -DGGML_OPENMP=OFF -DBUILD_SHARED_LIBS=OFF \
   -DWHISPER_BUILD_TESTS=OFF -DWHISPER_BUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release ../
   make
 
   echo "Build for Android complete!"
 
-  artifact_path="$build_path/libwhisper.a"
+  rm $unity_project/Packages/com.whisper.unity/Plugins/Android/*.a
+
+  artifact_path="$build_path/src/libwhisper.a"
   target_path="$unity_project/Packages/com.whisper.unity/Plugins/Android/libwhisper.a"
   cp "$artifact_path" "$target_path"
+
+  artifact_path=$build_path/ggml/src
+  target_path=$unity_project/Packages/com.whisper.unity/Plugins/Android/
+  cp "$artifact_path"/*.a "$target_path"
+  cp "$artifact_path"/*/*.a "$target_path"
 
   echo "Build files copied to $target_path"
 }
